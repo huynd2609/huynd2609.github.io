701df7a8aa2dde48a06a520b0fb00a2443397861
Nguyễn Đình Huy <huynguyendinh2609@gmail.com>
2019-12-24 22:59:09 +0700

    upload

--- coffee/vendor/js/login.js
+++ coffee/vendor/js/login.js
@@ -1,37 +1,34 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyA6r1hF8uK9ipnHb-5Uzm5-hAGCFoITfeY",
-    authDomain: "form-1ebd8.firebaseapp.com",
-    databaseURL: "https://form-1ebd8.firebaseio.com",
-    projectId: "form-1ebd8",
-    storageBucket: "form-1ebd8.appspot.com",
-    messagingSenderId: "210067598066",
-    appId: "1:210067598066:web:7d0844496d37acc5bebe6b"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+//TODO: PAGE LOGIN
+	// Your web app's Firebase configuration
+	var firebaseConfig = {
+		apiKey: "AIzaSyA6r1hF8uK9ipnHb-5Uzm5-hAGCFoITfeY",
+		authDomain: "form-1ebd8.firebaseapp.com",
+		databaseURL: "https://form-1ebd8.firebaseio.com",
+		projectId: "form-1ebd8",
+		storageBucket: "form-1ebd8.appspot.com",
+		messagingSenderId: "210067598066",
+		appId: "1:210067598066:web:f00a4350d57803b0bebe6b"
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
+	const auth = firebase.auth();
 
-function signIn() {
-    var userEmail = $('#email_field').val();
-    var userPass = $('#password_field').val();
+	function signIn() {
+		var userEmail = $('#email_field').val();
+		var userPass = $('#password_field').val();
+
+		const promise = auth.signInWithEmailAndPassword(userEmail, userPass);
+        promise.catch(e => alert(e.message));
+        
+	};
     
-    const promise = auth.signInWithEmailAndPassword(userEmail, userPass);
-    promise.catch(e => alert(e.message));
-};
-
-function signOut() {
-    auth.signOut();
-    $('html').load('index.html');
-};
-
-auth.onAuthStateChanged(function(user) {
-    if (user) {
-        $('html').load('index2.html');
-        var email = user.email;
-        alert("Active User " + email);
-    } else {
-        alert("No Active User");
-    }
-});
+	auth.onAuthStateChanged(function (user) {
+		if (user) {
+            window.location = "index.html";
+			var email = user.email;
+			console.log("Active User " + email);
+		} else {
+			console.log("No Active User");
+		}
+	});
