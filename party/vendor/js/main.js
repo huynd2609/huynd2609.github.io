f77a0527fca5d7959ad985c2ba21be6ccc9bb241
Nguyễn Đình Huy <huynguyendinh2609@gmail.com>
2020-03-20 14:20:39 +0700

    upload

--- party/vendor/js/main.js
+++ party/vendor/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
   }
 
   // Set the date we're counting down to
-  var countDownDate = new Date("Jan 01, 2020 00:00:00").getTime();
+  var countDownDate = new Date("Jan 01, 2021 00:00:00").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function () {
